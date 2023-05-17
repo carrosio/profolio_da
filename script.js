d18ce55e367848ec3e18a4890a907184bd8703b8
pepe <imcarrosio@gmail.com>
2023-05-17 13:59:07 -0300

    basic made

--- script.js
+++ script.js
@@ -7,3 +7,11 @@ navLinks.forEach((link) => {
     targetSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
   });
 });
+
+
+<p class="text-lg w-full mb-8 md:text-2xl md:mb-8 md:pr-16">
+              I am Mauricio Molina, a passionate and ambitious data analyst driven to explore the world of data and conquer new horizons.
+              Since childhood, I have had an insatiable curiosity and a relentless pursuit of excellence. I am determined to become the best in my field, pushing boundaries and leaving an indelible mark.
+              With my analytical prowess, I transform raw data into valuable insights, empowering decision-makers. My empathetic nature allows me to understand the human element behind the data, creating impactful solutions.
+              Beyond data analysis, I embrace challenges, staying ahead of emerging trends. Let's embark on a journey to unlock the limitless possibilities of data and create a transformative future.
+            </p>
